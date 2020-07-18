6542a309282c286e48c69d6eaf500b3aa2bff202
Lelianto <wildan.firdaus@gudangada.com>
2020-07-18 13:03:16 +0700

    add mainPhoto view

---
+++ cermati-test/src/components/serviceCard.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import '../styles/css/main.css'
+import '../styles/css/bootstrap.min.css';
+
+class Bottomheader extends Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+		};
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<div className='container-fluid'>
+					<div className='row'>
+					</div>
+				</div>
+			</React.Fragment>
+		)
+	} 
+}
+
+export default Bottomheader;
